c8b59e1a65a0911cff8fbe9611ef4565e9a3384f
hexNulgath <machadoignacio@hotmail.com>
2024-06-13 13:36:42 +0000

    added task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'runs a text'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char* text = "\"Programming is like building a multilingual puzzle";
+	char *text = "\"Programming is like building a multilingual puzzle";
+
 	puts(text);
 	return (0);
 }
