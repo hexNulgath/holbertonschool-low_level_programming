6b4a031f4076164e65d2c22a498f82d334f26800
hexNulgath <machadoignacio@hotmail.com>
2024-06-13 16:35:27 +0000

    started new repo

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: 'prints if number is positive or negativa'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if (n>=0){
+		printf("%lu is positive\n", n);
+	}else{
+		printf("%lu is negative\n", n);
+	}
+	return (0);
+}
