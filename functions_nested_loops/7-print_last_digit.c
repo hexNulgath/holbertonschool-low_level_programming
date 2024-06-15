d03a915a91791e76b7ebf42d5c98d97041b192d3
hexNulgath <machadoignacio@hotmail.com>
2024-06-15 03:41:26 +0000

    added task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * @n: acepts a int
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	r = n % 10;
+	_putchar(r);
+	return (r);
+}
