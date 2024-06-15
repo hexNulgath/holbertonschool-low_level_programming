4cac15cff383c32b588e132249ce06dbe7060980
hexNulgath <machadoignacio@hotmail.com>
2024-06-15 02:36:30 +0000

    added task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isalpha - checks alphabetic characters
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	if (c > 96 && c < 122 || c > 64 && c < 90)
+		return (1);
+	else
+		return (0);
+}
