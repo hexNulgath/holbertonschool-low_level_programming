02b5076567fd4cb9dfc57efd0de2477078c3f134
hexNulgath <machadoignacio@hotmail.com>
2024-06-15 04:40:38 +0000

    added task 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -6,17 +6,14 @@
  */
 int print_last_digit(int n)
 {
-	unsigned int r;
+    int last_digit = n % 10;
 
-	if (n < 0)
-	{
-		n = -n;
-		r = n % 10;
-	}
-	else
-	{
-		r = n % 10;
-	}
-	_putchar(r+48);
-	return (r);
+    if (last_digit < 0)
+    {
+        last_digit = -last_digit;
+    }
+
+    _putchar(last_digit + '0'); 
+
+    return (last_digit);
 }
