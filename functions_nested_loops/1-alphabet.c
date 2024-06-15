2c06a57e81d5a33de6036717d844e9758bf1367a
hexNulgath <machadoignacio@hotmail.com>
2024-06-15 01:37:51 +0000

    added task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
  * print_alphabet - prints alphabet
- * 
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	char i = 'a';
 
-	while ( i <= 'z')
+	while (i <= 'z')
 	{
 		_putchar(i);
 		i++;
