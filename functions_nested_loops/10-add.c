b169bfa34949a0eb78b8bdf48b1455e879d52b01
hexNulgath <machadoignacio@hotmail.com>
2024-06-16 01:18:47 +0000

    added task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @a: an int to sum
+ * @b: an int to sum
+ * Return: a + b sum
+ */
+int add(int a, int b)
+{
+	int r = a + b;
+
+	return (r);
+}
