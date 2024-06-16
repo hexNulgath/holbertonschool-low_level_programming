c9b4085b619fd6566e3e050f4d874e7578f681c0
hexNulgath <machadoignacio@hotmail.com>
2024-06-16 14:27:00 +0000

    added task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "stdio.h"
 /**
  * print_to_98 - prints numbers until 98
  * @n: an int to start the count
@@ -6,7 +7,26 @@
  */
 void print_to_98(int n)
 {
-	
-
-	return (void);
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d,  ", n);
+			n++;
+		}
+		printf("%d", n);
+		putchar('\n');
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d,  ", n);
+			n--;
+		}
+		printf("%d", n);
+		putchar('\n');
+	}
+	else
+		 printf("%d\n", n);
 }
