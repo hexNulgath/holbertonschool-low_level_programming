55c02051bd4253d14346f0cdaf049d23d9ec1936
hexNulgath <machadoignacio@hotmail.com>
2024-06-17 15:38:46 +0000

    added task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal
+ * @n: number of \
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0;
+	int r = 0;
+
+	while (i < n)
+	{
+		if (n > 1)
+			while (r < i)
+			{
+				_putchar(' ');
+				r++;
+			}
+		_putchar(92);
+		_putchar('\n');
+		i++;
+		r = 0;
+	}
+	_putchar('\n');
+}
