876fe36ee574890857c921b801e2866cc6c97550
hexNulgath <machadoignacio@hotmail.com>
2024-06-17 13:52:59 +0000

    added task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies 2 int
+ * @a: int to multiply
+ * @b: int to multiply
+ * Return: returns r
+ */
+
+int mul(int a, int b)
+{
+	int r = a * b;
+	
+	return (r);
+}
