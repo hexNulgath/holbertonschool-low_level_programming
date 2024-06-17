3e1d4bb843b38645577e4c75a3ef395baf7191d7
hexNulgath <machadoignacio@hotmail.com>
2024-06-17 13:24:31 +0000

    added task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * _isupper - checks for uppercase
+ * @c: charcter to check if upper
  * Return: returns 1 if upper 0 otherwise (Success)
  */
 int _isupper(int c)
 {
 	if (c < '['  && c > '@')
 		return (1);
-	else 
+	else
 		return (0);
 }
