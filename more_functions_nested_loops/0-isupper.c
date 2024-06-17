0a491ecbad275cf0759b60e291cf73e2c8e10f96
hexNulgath <machadoignacio@hotmail.com>
2024-06-17 13:14:28 +0000

    added task 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase
+ * Return: returns 1 if upper 0 otherwise (Success)
+ */
+int _isupper(int c)
+{
+	if (c < 91  && c > 64)
+		return ('1');
+	else 
+		return ('0');
+}
