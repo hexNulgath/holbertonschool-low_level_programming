23e471c24536f877760d9cf775b754872ebf18db
hexNulgath <machadoignacio@hotmail.com>
2024-06-20 19:10:33 +0000

    added task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,21 +7,27 @@
 
 int _atoi(char *s)
 {
-	int res = 0;
-	int sign = 1;
-	int i = 0;
+	
+    int res = 0;
+    int sign = 1; 
+    int i = 0;
 
-	while (s[i] < '0' || s[i] > '9')
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		i++;
-	}
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		res = res * 10 + (s[i] - '0');
-		i++;
-	}
+    
+    while (s[i] == ' ' || s[i] == '+' || s[i] == '-')
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        i++;
+    }
 
-	return (res * sign);
+  
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        res = res * 10 + (s[i] - '0');
+        i++;
+    }
+
+  
+	return res * sign;
+
 }
