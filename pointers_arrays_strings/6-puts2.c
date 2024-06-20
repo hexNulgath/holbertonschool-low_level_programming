5f72dc89ce8c15deb9b424b94d73b69c4f1be6da
hexNulgath <machadoignacio@hotmail.com>
2024-06-20 16:50:13 +0000

    added task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -5,18 +5,19 @@
  * Return: void
  */
 
-void puts2(char *s)
+void puts2(char *str)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
 		{
-			_putchar(s[i]);
+			_putchar(str[i]);
 			i++;
 		}
 		else
 			i++;
 	}
+	_putchar('\n');
 }
