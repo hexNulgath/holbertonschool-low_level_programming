83d42c1ac2b1ad85c0501b481e0986ab7840f0d8
hexNulgath <machadoignacio@hotmail.com>
2024-06-20 14:40:51 +0000

    added task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,18 +8,20 @@
 void rev_string(char *s)
 {
 	int i = 0;
-	char * reverse[] = s;
+	char reverse;
         int r = 0;
 
 	while (s[i] != '\0')
 	{
 		i++;
 	}
+	reverse[i];
 	while (i > 0)
 	{
 		i--;
 		reverse[r] = s[i];
 		r--;
 	}
+	s = reverse;
 	_putchar('\n');
 }
