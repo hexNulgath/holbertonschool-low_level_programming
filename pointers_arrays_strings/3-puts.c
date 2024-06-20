42dce707e7cfe21313d4425678efacfc12409d59
hexNulgath <machadoignacio@hotmail.com>
2024-06-20 14:17:36 +0000

    added task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: a string 
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int length = 0;
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		length++;
+		i++;
+	}
+}
