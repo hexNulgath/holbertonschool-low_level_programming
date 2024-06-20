2539efde672a21e7691f45c58d68f0c0bd58c5c8
hexNulgath <machadoignacio@hotmail.com>
2024-06-20 18:36:32 +0000

    added task 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _atoi - converts a string to int
+ * @s: a string
+ * Return: returns a number or 0
+ */
+
+int _atoi(char *s)
+{
+	int res = 0;
+	int sign = 0;
+	int i = 0;
+	int start = 0;
+
+	while(s[i] < '0' || s[i] > '9')
+	{
+		start++;
+		i++;
+		if (s[i] == '+')
+			sign++;
+		else if (s[i] == '-')
+			sign--;
+	}
+	while(s[i] >= '0' && s[i] <= '9')
+	{
+		res = res*10 + (s[i] - '0');
+		i++;
+	}
+	if (sign < 0)
+		res = -res;
+
+	return (res);
+}
