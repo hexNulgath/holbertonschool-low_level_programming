b8ea1175e7b2e8cdc64283776bfca2a6f648a77d
hexNulgath <machadoignacio@hotmail.com>
2024-06-20 14:24:58 +0000

    added task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: a string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		length++;
+		i++;
+	}
+	while (length >= 0)
+	{
+		_putchar(s[length]);
+		length--;
+	}
+	_putchar('\n');
+}
