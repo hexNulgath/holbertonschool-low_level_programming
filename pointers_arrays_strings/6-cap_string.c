b0968b412cafa03d0c08d959ce3862c74ed2f8a5
hexNulgath <machadoignacio@hotmail.com>
2024-06-24 14:29:13 +0000

    added task 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -23,7 +23,7 @@ char *cap_string(char *s)
 				s[i] = s[i] - 32;
 			}
 		}
-		else if (s[i] == 44 || s[i] == 9 || s[i] == '10')
+		else if (s[i] == 44 || s[i] == 9 || s[i] == 10)
 		{
 			i++;
 			if (s[i] < 123 && s[i] > 96)
