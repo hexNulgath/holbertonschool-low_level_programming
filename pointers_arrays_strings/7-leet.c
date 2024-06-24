df1020e5f6af2bee97216fa80a7663e0b8a01518
hexNulgath <machadoignacio@hotmail.com>
2024-06-24 15:54:07 +0000

    added task 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * leet - encodes a string to 1337
+ * @s: a string
+ * Return: string of char
+ */
+
+char *leet(char *s)
+{
+	int i = 0;
+	char letters[5] = {'a', 'e', 'o', 't', 'l'}; 
+	int rep[5] = {4, 3, 0, 7, 1}; 
+	int r = 0;
+
+	while (s[i] != '\0')
+	{
+		while (r < 6)
+		{
+			if (s[i] == letters[r] || s[i] == letters[r] - 32)
+			{
+				s[i] = rep[r];
+			}	
+		}
+		r = 0;
+		i++;
+	}
+	return (s);
+}
