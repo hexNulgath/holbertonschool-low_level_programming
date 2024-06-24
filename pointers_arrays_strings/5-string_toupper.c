97f83f9acddcef15e47c51b98ac12010ce43be5e
hexNulgath <machadoignacio@hotmail.com>
2024-06-24 13:28:51 +0000

    added task 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - makes all char to uppercase
+ * @s: a character array
+ * Return: a string in upper
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s < 96 && s < 123)
+			s[i] = s[i] - 32;
+		i++;
+	}
+}
