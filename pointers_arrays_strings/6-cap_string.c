ceac5a8e66a2f8c768f2fa34bd9dd03d599ccfdd
hexNulgath <machadoignacio@hotmail.com>
2024-06-24 13:44:31 +0000

    added task 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,13 +9,13 @@ char *cap_string(char *s)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
-		if (i = 0 && s[i] < 123 && s[i] > 96)
+		if (i == 0 && s[i] < 123 && s[i] > 96)
 		{
 			s[i] = s[i] - 32;
 		}
-		else if (s[i] == 32)
+		else if (s[i] == 32 || s[i] == '\n' || s[i] == '\b')
 		{
 			i++;
 			s[i] = s[i] - 32;
