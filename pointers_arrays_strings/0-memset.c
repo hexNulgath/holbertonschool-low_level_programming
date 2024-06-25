083ff319194040b382451db51e64ff2306153023
hexNulgath <machadoignacio@hotmail.com>
2024-06-25 12:34:54 +0000

    added task 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -4,12 +4,12 @@
  * @s: a pointer to memory to fill
  * @b: byte for filling
  * @n: number of bytes to fill
- * Return: a pointer to memory area 
+ * Return: a pointer to memory area
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int unsigned i = 0;
+	unsigned int i = 0;
 
 	while (i < n)
 	{
