eb0814c18980866b97da1686d7bce39922cb86d5
hexNulgath <machadoignacio@hotmail.com>
2024-06-25 14:37:28 +0000

    added task 4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -10,23 +10,18 @@ char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i = 0;
 	unsigned int r = 0;
-	unsigned int z = 0;
-	char * pS;
+	char * pS = accept;
 
 	while (s[i] !=  '\0')
 	{
 		while (accept[r] != '\0')
 		{
-			if (accept[r] == s[i])
-				pS[z] = s[i];					
+			if (*pS == s[i])
+				return (char *)s;						
 			r++;
 		}
 		r = 0;
 		i++;
 	}
-
-	if (pS[0] == '\0') 
 		return ("nil");
-	else
-		return (pS);
 }
