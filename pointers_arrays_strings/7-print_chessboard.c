7cdee5b43ea9af31ba7ad7fa0f549f53a0ee5959
hexNulgath <machadoignacio@hotmail.com>
2024-06-25 18:51:03 +0000

    added task 7

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_chessboard - prints a chessboard
+ * @a: a array of pices and spaces
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+	int z = 0;
+
+	while (z < 9)
+	{
+		while (i < 9)
+		{
+			_putchar(a[z][i]);
+			i++;
+		}
+		_putchar('\n');
+		i = 0;
+		z++;
+	}
+}
