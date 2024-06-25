29226e5f4f36b42b30f35bba5d28c71182447fd7
hexNulgath <machadoignacio@hotmail.com>
2024-06-25 12:46:01 +0000

    added task 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - copy a memory area
+ * @dest: destination of copy
+ * @src: from to copy
+ * @n: number of bytes to copy
+ * Return: a memory pointer
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+}
