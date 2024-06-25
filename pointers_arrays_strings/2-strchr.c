28dcebe0204e1ab3c23e5c03d2d5bf657aaafafa
hexNulgath <machadoignacio@hotmail.com>
2024-06-25 12:56:29 +0000

    added task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: string to inspect
+ * @c: character to locate
+ * Return: pointer to c in s
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i] != c)
+	{
+		if (s[i] == '\0')
+			return ("NULL");
+		i++;
+	}
+	return (&s[i]);
+}
