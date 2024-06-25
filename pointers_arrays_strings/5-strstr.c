9a2671d0ccc29d62bfa1119a7f5d88a51ac05b2a
hexNulgath <machadoignacio@hotmail.com>
2024-06-25 14:02:03 +0000

    added task 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: string to find from
+ * @needle: string to find
+ * Return: returns a string pointing to  the beginin of substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0;
+	unsigned int r = 0;
+	char * pNeedle;
+
+	while (haystack[i] !=  '\0')
+	{
+		if (needle[0] == haystack[i])
+		{
+			pNeedle = &haystack[i];
+			while (needle[r] != '\0')
+			{
+				if (needle[r] == haystack[i])
+				{
+					r++;
+					i++;
+					if (needle[r] == '\0')
+						return (pNeedle);
+				}
+				else
+					break;
+			}
+		}
+		else
+			i++;
+	}
+	return ("nil");
+}
