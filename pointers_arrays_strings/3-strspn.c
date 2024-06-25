467293f4f564d6cb172ecbeabde4dcc2aca1d4b9
hexNulgath <machadoignacio@hotmail.com>
2024-06-25 13:13:44 +0000

    added task 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strspn - gets lenght of a prefixsubstring
+ * @s: string to examin
+ * @accept: characters to acept
+ * Return: number bytes at start of s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	unsigned int r = 0;
+
+	while (accept[r] !=  '\0')
+	{
+		if (accept[r] == s[i])
+		{
+			i++;
+			r = 0;
+		}
+		else
+			r++;
+	}
+	return(i);
+}
