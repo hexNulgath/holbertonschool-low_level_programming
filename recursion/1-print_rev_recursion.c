83757698715c51b1a3dd3f215187be03652ae084
hexNulgath <machadoignacio@hotmail.com>
2024-06-26 14:43:47 +0000

    added task 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in revers
+ * @s: a string to print
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	s++;
+	_print_rev_recursion(s);
+	_putchar(*s);
+	s--;
+	if (*s == '\n')
+		_putchar('\n');
+	s++;
+}
