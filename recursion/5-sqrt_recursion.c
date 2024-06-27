25f1ce36453d6da93e0554c7b52c217bae3be40d
hexNulgath <machadoignacio@hotmail.com>
2024-06-27 14:30:08 +0000

    added task 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _root - returns pow of given number
+ * @n: a number to rise
+ * @y: an int for the power
+ * Return: the power of x to y
+ */
+int _root(int n, int y)
+{
+	if (y == 0)
+		return (-1);
+	if (n / y == y && n % y == 0)
+	{
+		return (y);
+	}
+	else
+	{
+		y--;
+		return (_root(n, y));
+	}
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: a number to root
+ * Return: return the root
+ */
+int _sqrt_recursion(int n)
+{
+	int y = n;
+	int f;
+
+	if (n < 0)
+		return (-1);
+	f = _root(n, y);
+
+	return (f);
+}
