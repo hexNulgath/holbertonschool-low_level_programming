2ead3f447f8f870b5f198285e4a68c5784cd4df9
hexNulgath <73831621+hexNulgath@users.noreply.github.com>
2024-06-27 10:30:41 -0300

    Create 3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * fact - returns factorial of given number
+ * @n: a number to factorize
+ * Return: factorial of n
+ */
+int fact(int n)
+{
+        if (n == 0)
+                return (1);
+        return (n * fact(n - 1));
+}
+
+/**
+ * factorial - returns factorial of given number
+ * @n: a number to factorize
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+        int f;
+
+        if (n < 0)
+                return (-1);
+        if (n == 0)
+                return (1);
+
+        f = fact(n);
+
+        return (f);
+}
