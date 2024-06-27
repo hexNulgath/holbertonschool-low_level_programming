737fbbd6b07fe5e582f821698cfa6a91c927f68f
hexNulgath <73831621+hexNulgath@users.noreply.github.com>
2024-06-27 09:51:36 -0300

    Update 2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -6,14 +6,15 @@
  */
 int _length(char *s, int n)
 {
-    if (*s == '\0')
-        return n;
-    n++;
-    return (_length(s + 1, n));
+if (*s == '\0')
+return (n);
+n++;
+return (_length(s + 1, n));
 }
 
 int _strlen_recursion(char *s)
 {
-    int n = 0;
-    return (_length(s, n));
+int n = 0;
+    
+return (_length(s, n));
 }
