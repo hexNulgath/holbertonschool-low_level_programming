45e66c9e2b1676f00b852ed599053631e0f2b186
hexNulgath <machadoignacio@hotmail.com>
2024-06-28 14:36:54 +0000

    added task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'prints its name'
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
