9c4cd2bae9ae06ca789150a7ef480bdbee003110
hexNulgath <machadoignacio@hotmail.com>
2024-07-01 16:51:57 +0000

    added task 4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free array
+ * @grid: grid array
+ * @height: size of height
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i = height;
+
+	while (i > 0)
+		free(p[--i]);
+	free(p);
+}
