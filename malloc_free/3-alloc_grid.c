62ff1bf4b8f277b072de3a75410c4f03b8b053eb
hexNulgath <machadoignacio@hotmail.com>
2024-07-01 16:30:29 +0000

    added task 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -19,9 +19,9 @@ int **alloc_grid(int width, int height)
 	while (i < height)
 	{
 		p[i] = malloc(width * sizeof(int));
-		i++;
 		if (p[i] == NULL)
 			return (NULL);
+		i++;
 	}
 	i = 0;
 	while (i < height)
