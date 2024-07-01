18329ea2a503f623fadd000abaefcee500c14bb0
hexNulgath <machadoignacio@hotmail.com>
2024-07-01 13:35:35 +0000

    added task 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -5,7 +5,7 @@
  * create_array - creates an array of chars
  * @size: size of array
  * @c: a char to initalize
- * return: NULL or a pointer to the array
+ * Return: NULL or a pointer to the array
  */
 
 char *create_array(unsigned int size, char c)
