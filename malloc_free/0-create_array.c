b759a01edab093d9dd3233ee7adaef64be6266c3
hexNulgath <machadoignacio@hotmail.com>
2024-07-01 13:10:14 +0000

    added task 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of array
+ * @c: a char to initalize
+ * return: NULL or a pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return NULL;
+	p = malloc((size + 1) * sizeof(char));
+	while (i <= size)
+	{
+		p[i] = c;
+		i++;
+	}
+
+	return (p);
+
+}
