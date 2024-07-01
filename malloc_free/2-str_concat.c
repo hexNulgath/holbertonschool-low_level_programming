6e1f68d4c399dc14fca93e72b46cef6474463d8e
hexNulgath <machadoignacio@hotmail.com>
2024-07-01 14:59:44 +0000

    added task 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: NULL or a pointer to the array
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	unsigned int i = 0;
+	unsigned int z = 0;
+	unsigned int size;
+
+	size = 0;
+	while (s1[i] != '\0')
+	{
+		i++;
+		size++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		i++;
+		size++;
+	}
+	i = 0;
+	p = malloc((size + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	while (s2[z] != '\0')
+	{
+		p[i] = s2[z];
+		i++;
+		z++;
+	}
+	if (s2[z] == '\0')
+		p[i] = s2[z];
+	return (p);
+}
