3a14250164a4f60315a9ac1bb425135c43950a9a
hexNulgath <machadoignacio@hotmail.com>
2024-07-02 15:02:35 +0000

    added task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * string_noncat - concatenates two strings
+ * string_nconcat - concatenates two strings
  * @s1: string 1
  * @s2: string 2
  * @n: number of bytes of s2 to reallocate
@@ -16,7 +16,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int size2;
 	unsigned int i;
 	unsigned int z;
-	
+
 	size1 = sizeof(s1);
 	size2 = sizeof(s2);
 	if (n > size2)
@@ -27,12 +27,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < size1 - 3; i++)
-		p[i] = s1[i];
-	for (z = 0; z < n && s2[z] != '\0'; z++)
+	if (s1 != "" &&s1 != NULL)
 	{
-		p[i] = s2[z];
-		i++;
+		for (i = 0; i < size1 - 3; i++)
+			p[i] = s1[i];
+	}
+	if (s2 != "" &&s2 != NULL)
+	{
+		for (z = 0; z < n && s2[z] != '\0'; z++)
+		{
+			p[i] = s2[z];
+			i++;
+		}
 	}
 	p[i] = '\0';
 	return (p);
