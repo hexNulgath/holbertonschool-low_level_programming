aac3ea35e38b1e67a56da985e379d5526823ddf7
hexNulgath <machadoignacio@hotmail.com>
2024-07-02 15:35:28 +0000

    added task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -17,28 +17,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0;
 	unsigned int z;
 
-	if (s1 == NULL || '\0')
+	if (s1 == NULL || s1[0] == '\0')
 		size1 = 0;
 	else
 		size1 = sizeof(s1) - 1;
-	if (s2 == NULL || '\0')
+	if (s2 == NULL || s1[0] == '\0')
 		size2 = 0;
 	else
 		size2 = sizeof(s2) - 1;
 	if (n > size2)
-		p = malloc(size1 + size2);
+		p = malloc((size1 + size2 + 1) * sizeof(char));
 	else
-		p = malloc(size1 + n);
+		p = malloc((size1 + n + 1) * sizeof(char));
 	if (p == NULL)
 	{
 		return (NULL);
 	}
-	if (s1[0] != '\0' && s1 != NULL)
+	if (s1 != NULL)
 	{
-		for (i = 0; i < size1 - 2; i++)
+		for (i = 0; i < size1 && s1[i] != '\0'; i++)
 			p[i] = s1[i];
 	}
-	if (s2[0] != '\0' && s2 != NULL)
+	if (s2 != NULL)
 	{
 		for (z = 0; z < n && s2[z] != '\0'; z++)
 		{
