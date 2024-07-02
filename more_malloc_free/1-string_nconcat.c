b023d26fa42dbb58c4ed82db3faaaa25607468d6
hexNulgath <machadoignacio@hotmail.com>
2024-07-02 15:15:50 +0000

    added task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -17,19 +17,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0;
 	unsigned int z;
 
-	size1 = sizeof(s1);
-	size2 = sizeof(s2);
-	if (n > size2)
-		p = malloc(size1 + size2 - 2);
+	if (s1 == NULL || '\0')
+		size1 = 0;
 	else
-		p = malloc(size1 + n - 2);
+		size1 = sizeof(s1) - 1;
+	if (s2 == NULL || '\0')
+		size2 = 0;
+	else
+		size2 = sizeof(s2) - 1;
+	if (n > size2)
+		p = malloc(size1 + size2);
+	else
+		p = malloc(size1 + n);
 	if (p == NULL)
 	{
 		return (NULL);
 	}
 	if (s1[0] != '\0' && s1 != NULL)
 	{
-		for (i = 0; i < size1 - 3; i++)
+		for (i = 0; i < size1 - 2; i++)
 			p[i] = s1[i];
 	}
 	if (s2[0] != '\0' && s2 != NULL)
