729b90d40f42cc8d2a99bdf5fd59deac24349843
hexNulgath <machadoignacio@hotmail.com>
2024-07-03 14:25:50 +0000

    added task 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory
+ * @size: size of each element
+ * @nmemb: number of elements
+ * Return: pointer to the reserved
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char * p;
+	if (nmemb == 0  || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	return (p);
+}
