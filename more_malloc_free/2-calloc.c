abf9a3b0e6447958e6f2173f828362d6042b41e9
hexNulgath <machadoignacio@hotmail.com>
2024-07-03 14:28:17 +0000

    added task 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,10 +9,13 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char * p;
+	unsigned int i = 0, j = nmemb;
 	if (nmemb == 0  || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
+	while (i < j)
+		p[i] = 0;
 	return (p);
 }
