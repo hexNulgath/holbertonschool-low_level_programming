c438634240e5a022223638d24c3e0b81c6a88a32
hexNulgath <machadoignacio@hotmail.com>
2024-07-04 20:19:26 +0000

    added task 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -10,8 +10,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	dog = d;
-	dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
