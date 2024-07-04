b8749e357b38aed6a7401011073ce3566f5d106a
hexNulgath <machadoignacio@hotmail.com>
2024-07-04 19:25:56 +0000

    added task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - data of a dog
+ * @owner: name of the owner
+ * @age: age of the dog
+ * @name: name of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
