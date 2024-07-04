a749c2e509f7c3a3054a1ce7a243d421bc42643d
hexNulgath <machadoignacio@hotmail.com>
2024-07-04 20:43:30 +0000

    added task 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - prints a list of structure d
+ * @d: pointer to structure
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: nil\n");
+	else
+		printf("Name: %s\n", d->name);
+	if (d->age == 0x00)
+		printf("Age: nil\n");
+	else
+		printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: nil\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
