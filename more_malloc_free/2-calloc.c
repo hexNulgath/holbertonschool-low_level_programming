619658453381b7008424afcbcb3d47dc9e194df0
hexNulgath <73831621+hexNulgath@users.noreply.github.com>
2024-07-04 11:53:42 -0300

    Update 2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb <= 0  || size <= 0)
 		return (NULL);
-	p = malloc(nmemb * size);
+	p = malloc(nmemb * size * sizeof(char));
 	if (p == NULL)
 		return (NULL);
 	while (i < nmemb)
