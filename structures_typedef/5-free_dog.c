819d003c7a24850ac4261a05e6c22914e92e0f09
hexNulgath <machadoignacio@hotmail.com>
2024-07-05 18:22:28 +0000

    fixed betty mistakes

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dog - frees dogs
+ * @d: pointer to dogs
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return (NULL);
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
