38f953a7d8616b3de5b202604a6e8f229060f59b
hexNulgath <machadoignacio@hotmail.com>
2024-07-05 16:37:44 +0000

    added task 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ * Return: NULL if the function fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	new_dog->name = name;
+	new_dog->age = age;
+}
