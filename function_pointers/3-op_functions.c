eb6066d4dd7f319d637d04c7cf3498ee861dfc55
hexNulgath <machadoignacio@hotmail.com>
2024-07-08 19:53:21 +0000

    set up task 3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: the elements to use
+ * @size: size of the array
+ * @cmp: function to be used to compare values
+ * Return: return index of first match or -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int (*compare)(int) = cmp;
+	int i;
+	int result;
+
+	if (size <= 0 || array == NULL || compare == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		result = compare(array[i]);
+		if (result != 0)
+			return (i);
+	}
+	return (-1);
+}
