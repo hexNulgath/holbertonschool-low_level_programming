c615ce86e16db410c86c7f44f98347260abc452b
hexNulgath <machadoignacio@hotmail.com>
2024-07-09 17:47:51 +0000

    added task 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -8,10 +8,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-
+	
 	va_start(args, n);
 	for (i = 0; i < n - 1 ; i++)
+	{
+		if (separator == NULL)
+			printf("%i", va_arg(args, int));
+		else
 		printf("%i%s", va_arg(args, int), separator);
+	}
 	printf("%i\n", va_arg(args, int));
 	va_end(args);
 }
