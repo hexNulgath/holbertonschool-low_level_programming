ff3ad1636a9934410464ea74c2f1e035c740434e
hexNulgath <machadoignacio@hotmail.com>
2024-07-09 17:40:11 +0000

    added task 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,17 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints numbers
+ * @n: size of array
+ * @separator: separator between numbers
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+	for (i = 0; i < n - 1 ; i++)
+		printf("%i%s", va_arg(args, int), separator);
+	printf("%i\n", va_arg(args, int));
+	va_end(args);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
 #include <stdarg.h>
+#include <stdio.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
