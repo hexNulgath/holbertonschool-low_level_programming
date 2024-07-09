08c7e10e93a06323f91138c3be9cb97748a268c6
hexNulgath <machadoignacio@hotmail.com>
2024-07-09 19:58:59 +0000

    added task 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -12,30 +12,21 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n - 1; i++)
 	{
-		if (separator == NULL)
+		char *arg = va_arg(args, char *);
+
+		if (arg == NULL)
 		{
-			if (va_arg(args, char *) == NULL)
-			{
-				printf("(nil)");
-			}
-			printf("%s", va_arg(args, char *));
-		}
-		else if (va_arg(args, char *) == NULL)
-		{
-			printf("(nil)%s", separator);
+			printf("(nil)");
 		}
 		else
 		{
-		printf("%s%s", va_arg(args, char *), separator);
+			printf("%s", arg);
+		}
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
 		}
 	}
-	if (va_arg(args, char *) == NULL)
-	{
-		printf("(nil\n)");
-	}
-	else
-	{
-		printf("%s\n", va_arg(args, char *));
-	}
+	printf("\n");
 	va_end(args);
 }
