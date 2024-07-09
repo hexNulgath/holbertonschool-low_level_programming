3e5153f499f3d340574db446ab14149cf87a58dd
hexNulgath <machadoignacio@hotmail.com>
2024-07-09 13:43:18 +0000

    added task 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,24 +1,23 @@
-#include "function_pointers.h"
+#include "3-calc.h"
 /**
- * int_index - searches for an integer
- * @array: the elements to use
- * @size: size of the array
- * @cmp: function to be used to compare values
+ *get_op_func - executes correct operation 
+ *@s: operator to be used
  * Return: return index of first match or -1
  */
-int int_index(int *array, int size, int (*cmp)(int))
+int (*get_op_func(char *s))(int, int)
 {
-	int (*compare)(int) = cmp;
-	int i;
-	int result;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-	if (size <= 0 || array == NULL || compare == NULL)
-		return (-1);
-	for (i = 0; i < size; i++)
-	{
-		result = compare(array[i]);
-		if (result != 0)
-			return (i);
-	}
-	return (-1);
+	while (ops[i, 0] != s)
+		i++;
+
+	return (ops[i, 1]);
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -8,12 +8,26 @@
 int main(int argc, char *argv[])
 {
 	int number1 = int atoi(argv[argc - 3]);
+	char operator = argv[argc - 2]
 	int number2 = int atoi(argv[argc - 1]);
+	int result;
 
-	if (argc < 4 || argv[argc - 2] != '+' && argv[argc - 2] != '-' && argv[argc - 2] != '/' && argv[argc - 2] != '*')
+	if (argc < 4)
 	{
 		printf(Error\n);
 		exit(98);
 	}
-
+	if (operator != '+' && operator != '-' && operator != '/' && operator != '*' && operator != '%')
+        {
+                printf(Error\n);
+                exit(99);
+        }
+	if ((operator == '%' || operator != '/') && number2 == 0)
+	{
+		printf(Error\n);
+		exit(100);
+	}
+	result = get_op_func(operator)(number1, number2);
+	printf("%i\n",result);
+	return ('0');
 }
