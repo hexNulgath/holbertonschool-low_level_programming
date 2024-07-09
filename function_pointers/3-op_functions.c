006dbd88973a388b7af01dd50c491ea575caab48
hexNulgath <machadoignacio@hotmail.com>
2024-07-09 13:04:04 +0000

    rapidooooo

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,26 +1,51 @@
 #include "3-calc.h"
 /**
- */
+*op_add - addition
+*@a: int
+*@b: int
+*Return: result of operation
+*/
 int op_add(int a, int b)
 {
+	return (a + b);
 }
 /**
- */
+*op_sub - substraction
+*@a: int
+*@b: int
+*Return: result of operation
+*/
 int op_sub(int a, int b)
 {
+	return (a - b);
 }
 /**
- */
+*op_mul - multiply
+*@a: int
+*@b: int
+*Return: result of operation
+*/
 int op_mul(int a, int b)
 {
+	return (a * b);
 }
 /**
- */
+*op_div - division
+*@a: int
+*@b: int
+*Return: result of operation
+*/
 int op_div(int a, int b)
 {
+	return (a / b);
 }
 /**
- */
+*op_mod - module
+*@a: int
+*@b: int
+*Return: result of operation
+*/
 int op_mod(int a, int b)
 {
+	return ( a % b);
 }
