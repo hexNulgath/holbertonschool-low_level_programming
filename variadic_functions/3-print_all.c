6a4cd09f6516a061c3106a1bfcbe378b2ca5c7d3
hexNulgath <machadoignacio@hotmail.com>
2024-07-10 14:11:11 +0000

    added task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -5,13 +5,15 @@
  */
 void print_all(const char * const format, ...)
 {
-	char formaters[4];
+	char formaters[4] = {'\0'};
 	int i = 0;
 	int j = 0;
+	va_list args;
 
+	va_start(args, n);
 	while (format[i] != '\0')
 	{
-		switch (format) {
+		switch (format[i]) {
 			case 'i':
 				formaters[j] = 'i';
 				j++;
@@ -31,4 +33,30 @@ void print_all(const char * const format, ...)
 		}
 		i++;
 	}
+	i = 0;
+	j = 0;
+	while (formaters[i] != '\0')
+	{
+		switch (formaters[i]){
+			case 'i':
+				printf("%i", va_arg(args, int));
+				j++;
+				break;
+			case 'c':
+				printf("%c", va_arg(args, char));
+				j++;
+				break;
+			case 'f':
+				printf("%f", va_arg(args, float));
+				j++;
+				break;
+			case 's':
+				printf("%s", va_arg(args, char*));
+				j++;
+				break;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
 }
