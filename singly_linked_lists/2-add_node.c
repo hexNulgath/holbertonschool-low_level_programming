1b418f2b188a9240eea1b77bdc2d4574c87da93b
hexNulgath <machadoignacio@hotmail.com>
2024-07-29 23:33:26 +0000

    added task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,5 +1,23 @@
 #include "lists.h"
 /**
+ * _strlen - total of characters in string
+ * @s: a string pointer
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		length++;
+		i++;
+	}
+	return (length);
+}
+/**
  * add_node -  adds a new node at the beginning of a list
  * @head: the head of a linked list
  * @str: string to copy on new node
@@ -8,7 +26,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	struct list_s *lk = malloc(sizeof(struct list_s));
-	lk->len = strlen(str);
+	lk->len = _strlen(str);
 	lk->str = strdup(str);
 	lk->next = *head;
 	*head = lk;
