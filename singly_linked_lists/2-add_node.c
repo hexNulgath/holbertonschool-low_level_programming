175d482b01140db5b0b45c481f2ba9a1da90553f
hexNulgath <machadoignacio@hotmail.com>
2024-07-29 23:37:06 +0000

    added task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -17,6 +17,7 @@ int _strlen(const char *s)
 	}
 	return (length);
 }
+
 /**
  * add_node -  adds a new node at the beginning of a list
  * @head: the head of a linked list
@@ -26,12 +27,12 @@ int _strlen(const char *s)
 list_t *add_node(list_t **head, const char *str)
 {
 	struct list_s *lk = malloc(sizeof(struct list_s));
+
+	if (lk == NULL)
+		return (NULL);
 	lk->len = _strlen(str);
 	lk->str = strdup(str);
 	lk->next = *head;
 	*head = lk;
-	if (lk == NULL)
-		return (NULL);
-	else
-		return (lk);
+	return (lk);
 }
