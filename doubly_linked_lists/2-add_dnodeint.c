87b30377af07a1ea46b0677c45b736ec913f56ff
hexNulgath <machadoignacio@hotmail.com>
2024-07-31 19:55:05 +0000

    added task 2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * add_dnodeint -  returns elements of a list
+ * @h: the head of a linked list
+ * Return: the adress of new nodes
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+	
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
+	return (new);
+}
