8091fa15c1617e007f8d0eefc70888d36559f68a
hexNulgath <machadoignacio@hotmail.com>
2024-07-31 21:20:36 +0000

    added task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		current = current->next;
 	}
-	if (current == NULL)
+	if (current == NULL || idx < 0)
 		return (NULL);
 
 	new->prev = current;
