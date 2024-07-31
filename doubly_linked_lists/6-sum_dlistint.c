39e6c17b46716e548ce5422fff73d4ce751dde78
hexNulgath <machadoignacio@hotmail.com>
2024-07-31 20:45:00 +0000

    added task 6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_dlistint -  sum of all the data
+ * @head: the head of a linked list
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
