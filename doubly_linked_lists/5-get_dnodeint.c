aeb560357ec8ef9c8b23647c0aa7e49413619575
hexNulgath <machadoignacio@hotmail.com>
2024-07-31 20:37:15 +0000

    added task 5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -3,7 +3,7 @@
  * get_dnodeint_at_index -  returns the nth node of a list
  * @head: the head of a linked list
  * @index: index of the node
- * return: ponter to node
+ * Return: ponter to node
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
