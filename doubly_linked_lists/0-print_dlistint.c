d9c73c987675c72a305f448fbd6afd5aeeb7ad92
hexNulgath <machadoignacio@hotmail.com>
2024-07-31 17:27:22 +0000

    added task 0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,18 @@
 #include "lists.h"
-
+/**
+ * print_dlistint -  prints all the elements of a list
+ * @h: the head of a linked list
+ * Return: the number of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-	struct list_s *lk = malloc(sizeof(struct list_s));
+	int n = 0;
+
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
 }
