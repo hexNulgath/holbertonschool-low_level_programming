7c974b96bc10fbf481a6ee8f6825a8735a9bcd27
hexNulgath <73831621+hexNulgath@users.noreply.github.com>
2024-08-01 17:40:45 -0300

    Update 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,28 +9,27 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i;
-	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *new;
 	dlistint_t *current = *h;
 
-	if (new == NULL)
-		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	new->n = n;
-
-	for (i = 0; current != NULL && i < idx - 1; i++)
+	for (i = 0; i < idx - 1; i++)
 	{
+		if (current == NULL)
+			return (NULL);
 		current = current->next;
 	}
 	if (current == NULL)
+		return (add_dnodeint_end(h, n));
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
-
+	new->n = n;
 	new->prev = current;
 	new->next = current->next;
 	current->next = new;
-
 	if (current->next != NULL)
 		current->next->prev = new;
-
 	return (new);
 }
