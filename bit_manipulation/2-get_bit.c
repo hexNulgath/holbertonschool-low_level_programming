f482a19201c243902eb0ee6b607659240e0db91d
hexNulgath <machadoignacio@hotmail.com>
2024-08-03 06:35:59 +0000

    added task 2

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get_bit - prints binary
+ * @n: a decimal number
+ * @index: bit you want to get
+ * Return: value of the bit at index index or -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int i;
+
+	i = 1UL << index;
+	if (n & i)
+		return (1);
+	else
+		return (0);
+}
