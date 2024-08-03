76343b3511469f84c5612ebf2a12cda83875bfb1
hexNulgath <machadoignacio@hotmail.com>
2024-08-03 07:18:47 +0000

    added task 5

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * flip_bits - number of difference of bits
+ * @n: a decimal number
+ * @m: a decimal number
+ * Return: number of bits you would need to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i, count = 0;
+	unsigned long int bit_n, bit_m;
+
+	for (i = 0; i < 63; i++)
+	{
+		bit_n = (n >> i) & 1;
+		bit_m = (m >> i) & 1;
+		if (bit_n != bit_m)	
+		count++;
+	}
+	return (count);
+}
