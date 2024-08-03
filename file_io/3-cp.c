e543e8baed0cdea461339835236eb8b764259dc2
hexNulgath <machadoignacio@hotmail.com>
2024-08-03 03:28:09 +0000

    added task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,58 +7,64 @@
  */
 void cp(const char *file_from, const char *file_to)
 {
-	int fd, fd1;
-	ssize_t r_res;
-	char buffer[1024];
+    int fd, fd1;
+    ssize_t r_res;
+    char buffer[1024];
 
-	fd = open(file_from, O_RDONLY);
-	if (fd == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-	fd1 = open(file_to, O_WRONLY | O_CREAT | O_EXCL, 0664);
-	if (fd1 == -1)
-	{
-		if (errno == EEXIST)
-			fd1 = open(file_to, O_WRONLY | O_TRUNC);
-		if (fd1 == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", file_to);
-			close(fd);
-			exit(99);
-		}
-	}
-	while ((r_res = read(fd, buffer, sizeof(buffer))) > 0)
-	{
-		if (write(fd1, buffer, r_res) != r_res)
-		{
-			dprintf(2, "Error: Can't write to %s\n", file_to);
-			close(fd);
-			close(fd1);
-			exit(99);
-		}
-	}
-	if (r_res == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		close(fd);
-		close(fd1);
-		exit(98);
-	}
-	if (close(fd) == -1)
-	{
-		dprintf(2, "Error: Can't close file descriptor %d\n", fd);
-		close(fd1);
-		exit(100);
-	}
-	if (close(fd1) == -1)
-	{
-		dprintf(2, "Error: Can't close file descriptor %d\n", fd1);
-		exit(100);
-	}
+    fd = open(file_from, O_RDONLY);
+    if (fd == -1)
+    {
+        dprintf(2, "Error: Can't read from file %s\n", file_from);
+        exit(98);
+    }
+
+    fd1 = open(file_to, O_WRONLY | O_CREAT | O_EXCL, 0664);
+    if (fd1 == -1)
+    {
+        if (errno == EEXIST)
+        {
+            fd1 = open(file_to, O_WRONLY | O_TRUNC);
+        }
+        if (fd1 == -1)
+        {
+            dprintf(2, "Error: Can't write to %s\n", file_to);
+            close(fd);
+            exit(99);
+        }
+    }
+
+    while ((r_res = read(fd, buffer, sizeof(buffer))) > 0)
+    {
+        if (write(fd1, buffer, r_res) != r_res)
+        {
+            dprintf(2, "Error: Can't write to %s\n", file_to);
+            close(fd);
+            close(fd1);
+            exit(99);
+        }
+    }
+
+    if (r_res == -1)
+    {
+        dprintf(2, "Error: Can't read from file %s\n", file_from);
+        close(fd);
+        close(fd1);
+        exit(98);
+    }
+
+    if (close(fd) == -1)
+    {
+        dprintf(2, "Error: Can't close file descriptor %d\n", fd);
+        close(fd1);
+        exit(100);
+    }
+
+    if (close(fd1) == -1)
+    {
+        dprintf(2, "Error: Can't close file descriptor %d\n", fd1);
+        exit(100);
+    }
 }
-
 int main(int ac, char **av)
 {
 
