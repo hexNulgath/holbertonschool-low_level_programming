0dc9df550631a7d5ce6e5fc7253345fcba1c7895
hexNulgath <machadoignacio@hotmail.com>
2024-08-03 00:05:58 +0000

    added task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -32,6 +32,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		write(STDOUT_FILENO, buf, letters);
 		free(buf);
+		close(fp);
 		return (letters);
 	}
 	 write(STDOUT_FILENO, buf, count);
