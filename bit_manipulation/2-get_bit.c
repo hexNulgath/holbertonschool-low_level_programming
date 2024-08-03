bb6344e373170ae35e981eef52ffa1b4b840c418
hexNulgath <machadoignacio@hotmail.com>
2024-08-03 06:41:58 +0000

    added task 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -8,9 +8,9 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int i;
-	
+
 	if (index >= 64)
-			return (-1);
+		return (-1);
 	i = 1UL << index;
 	if (n & i)
 		return (1);
