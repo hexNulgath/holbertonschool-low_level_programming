08dcfce07ebfa5a31ba890cd86549919ae9ed15b
hexNulgath <machadoignacio@hotmail.com>
2024-08-03 05:10:17 +0000

    added task 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,19 @@
+/**
+ * binary_to_uint - turns binary to int
+ * @b: a binary number
+ * Return: te converted ecimal number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, n = 0;
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		n = n * 2;
+		if (b[i] == '1')
+			n++;
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+	}
+	return (n);
+}
