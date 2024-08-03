a327024b469b0b2ba012eea5c86af96cb12bf206
hexNulgath <machadoignacio@hotmail.com>
2024-08-03 07:19:44 +0000

    added task 5

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -10,11 +10,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int i, count = 0;
 	unsigned long int bit_n, bit_m;
 
-	for (i = 0; i < 63; i++)
+	for (i = 0; i < 64; i++)
 	{
 		bit_n = (n >> i) & 1;
 		bit_m = (m >> i) & 1;
-		if (bit_n != bit_m)	
+		if (bit_n != bit_m)
 		count++;
 	}
 	return (count);
