7e594e2bccbad2ef57c926fce20683be12f5ba80
hexNulgath <machadoignacio@hotmail.com>
2024-08-03 00:31:35 +0000

    added task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,7 +9,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 
-	fd = open(filename, O_WRONLY | O_CREAT, 600);
+	fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (write(fd, text_content, strlen(text_content)) == -1)
