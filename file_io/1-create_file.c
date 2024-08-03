f2cf7343d6a39c43d3e226c29c07a870293d3479
hexNulgath <machadoignacio@hotmail.com>
2024-08-03 00:48:19 +0000

    added task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		if (errno == EEXIST)
 		{
-			fd = open(filename, O_WRONLY);
+			fd = open(filename, O_WRONLY | O_TRUNC);
 			if (fd == -1)
 				return (-1);
 		}
