1897a88dd42e47649f8c4f6fe6d7a36dc8b45dac
hexNulgath <machadoignacio@hotmail.com>
2024-08-03 00:24:59 +0000

    added task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: path to file
+ * @text_content: content for the new file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	fd = open(filename, O_WRONLY | O_CREAT, 600);
+	if (fd == -1)
+		return (-1);
+	if (write(fd, text_content, strlen(text_content)) == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
